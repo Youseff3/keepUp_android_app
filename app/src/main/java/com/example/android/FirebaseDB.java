65080c6b653b780f0eabd3e547833f168d9fcdc7
carsonaziz <carsonaziz@rogers.com>
2022-11-22 15:58:39 -0500

    Added interface for interacting with firestore database

--- app/src/main/java/com/example/android/FirebaseDB.java
+++ app/src/main/java/com/example/android/FirebaseDB.java
@@ -1,4 +1,58 @@
 package com.example.android;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirebaseDB {
+    private final String TAG = "FIREBASE";
+
+    private FirebaseFirestore db;
+
+    public FirebaseDB() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void insertIntoDocument(String collectionName, String documentName, Map<String, Object> data) {
+        db.collection(collectionName).document(documentName)
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    public void getDocumentFromCollection(String collectionName, String documentName, OnCompleteListener onCompleteListener) {
+        DocumentReference docRef = db.collection(collectionName).document(documentName);
+        docRef.get().addOnCompleteListener(onCompleteListener);
+    }
+
+    public void getDocumentFromCollectionWhereEqualTo(String collectionName, String whereField, Object equalTo, OnCompleteListener onCompleteListener) {
+        Query query = db.collection(collectionName).whereEqualTo(whereField, equalTo);
+        query.get().addOnCompleteListener(onCompleteListener);
+    }
 }
--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.android;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -14,9 +18,18 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.android.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME="MainActivity";
@@ -26,5 +39,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+////////////////////// Example Code for interacting with Firestore //////////////////////////////////
+
+//        listView = (ListView) findViewById(R.id.testListView);
+
+//        FirebaseDB fbdb = new FirebaseDB();
+
+        // OnCompleteListener for specific document
+//        OnCompleteListener onCompleteListener = new OnCompleteListener() {
+//            @Override
+//            public void onComplete(@NonNull Task task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = (DocumentSnapshot) task.getResult();
+//                    if (document.exists()) {
+//                        Map<String, Object> data = document.getData();
+//                        Log.d(ACTIVITY_NAME, "DocumentSnapshot data: " + document.getData());
+//                        final ArrayList<String> list = new ArrayList <>();
+//                        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1, list);
+//                        listView.setAdapter(adapter);
+//                        for (Object value : data.values()) {
+//                            list.add(value.toString());
+//                        }
+//                        adapter.notifyDataSetChanged();
+//                    } else {
+//                        Log.d(ACTIVITY_NAME, "No such document");
+//                    }
+//                } else {
+//                    Log.d(ACTIVITY_NAME, "get failed with ", task.getException());
+//                }
+//            }
+//        };
+
+        // OnCompleteListener for document where field is equal to specific value
+//        OnCompleteListener onCompleteListener = new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    final ArrayList<String> list = new ArrayList <>();
+//                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1, list);
+//                    for (QueryDocumentSnapshot doc : task.getResult()) {
+//                        Map<String, Object> data = doc.getData();
+//                        listView.setAdapter(adapter);
+//                        for (Object value : data.values()) {
+//                            Log.d("FIREBASE", value.toString());
+//                            list.add(value.toString());
+//                        }
+//                    }
+//                    adapter.notifyDataSetChanged();
+//                } else {
+//                    Log.d(ACTIVITY_NAME, "Error getting documents:  ", task.getException());
+//                }
+//            }
+//        };
+
+//        fbdb.getDocumentFromCollection("groups", "group10", onCompleteListener);
+//        fbdb.getDocumentFromCollectionWhereEqualTo("appointment", "course", "CP470", onCompleteListener);
+
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("course", "CP470");
+//        data.put("day", "2022-10-05");
+//        data.put("time", "7:30PM");
+//        data.put("title", "Give me a B");
+//        data.put("userid", "5");
+//        fbdb.insertIntoDocument("appointment", "myAppointment", data);
     }
 }
